03fd45cc1c94579fa927d69a403c207284b67a32
EfranH25 <efranhimel25@gmail.com>
2026-03-13 18:11:11 -0400

    fin ch 9

--- ch9-error-handling/src/main.rs
+++ ch9-error-handling/src/main.rs
@@ -1,6 +1,34 @@
-use std::{fs::File, io::ErrorKind};
+use std::{fs::File, io::ErrorKind, io::Read, io};
+use std::error::Error;
 
-fn main() {
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let username_file_result = File::open("hello.txt");
+
+    // let mut username_file = match username_file_result {
+    //     Ok(file) => file,
+    //     Err(e) => return Err(e)
+    // };
+    // Question mark handles match logic of result similar as above code (how error is returned is different)
+    let mut username_file = username_file_result?;
+
+    let mut username = String::new();
+    // match username_file.read_to_string(&mut username) {
+    //     Ok(_) => Ok(username),
+    //     Err(e) => Err(e),
+    // }
+    username_file.read_to_string(&mut username)?;
+    Ok(username)
+
+    // even simpler way via chaining:
+    // File::open("hello.txt")?.read_to_string(&mut username)?;
+    // Ok(username)
+}
+
+fn last_char_of_first_line(text: &str) -> Option<char> {
+    text.lines().next()?.chars().last()
+}
+fn main() -> Result<(),Box< dyn Error>>{
     let v = vec![1, 2, 3];
 
     // when reading backtrace --> lines before spot where code failed are lines your code called
@@ -27,4 +55,10 @@ fn main() {
         .expect("hello.txt should be included in this project");
 
     // in production most use .expect instead of .unwrap b/c it gives more context
+
+
+    let greeting_file = File::open("hello.txt")?;
+
+    Ok(())
+
 }
