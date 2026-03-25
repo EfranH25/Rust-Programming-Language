710f49e149cf94b553093b1282a7ed057497ff87
EfranH25 <efranhimel25@gmail.com>
2026-03-25 01:42:12 -0400

    using struct

--- ch12-io-project-cli/minigrep/src/main.rs
+++ ch12-io-project-cli/minigrep/src/main.rs
@@ -1,17 +1,27 @@
 use std::env;
 use std::fs;
 
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+    Config { query, file_path }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect(); // collect() is one of those rare functions in rust where you need to annotate
 
-    dbg!(&args);
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = parse_config(&args);
 
-    println!("Searching for \"{}\"", query);
-    println!("In file \"{}\"", file_path);
+    println!("Searching for \"{}\"", config.query);
+    println!("In file \"{}\"", config.file_path);
 
-    let contents = fs::read_to_string(file_path).expect("Something went wrong reading the file");
+    let contents =
+        fs::read_to_string(config.file_path).expect("Something went wrong reading the file");
 
     println!("With text:\n {}", contents);
 }
