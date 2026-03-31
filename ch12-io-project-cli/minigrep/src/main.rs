f58255be9f5bd85a99c4ff5ad6f37eb20d5e27e7
EfranH25 <efranhimel25@gmail.com>
2026-03-31 00:21:17 -0400

    added case sens + error prints

--- ch12-io-project-cli/minigrep/src/main.rs
+++ ch12-io-project-cli/minigrep/src/main.rs
@@ -1,28 +1,37 @@
 use std::{env, error::Error, fs, process};
-use minigrep::search;
+use minigrep::{search, search_case_insensitive};
 
-struct Config {
-    query: String,
-    file_path: String,
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
     fn build(args: &[String]) -> Result<Config, &'static str> {
         // cloning for simplicity > dirty way to fix ownership issues > others better ways to fix
-        if args.len() < 2 {
-            return Err("ERROR: Requires 2 arguments!");
+        if args.len() < 3 {
+            return Err("ERROR: Not  enough arguments!");
         }
 
         let query = args[1].clone();
         let file_path = args[2].clone();
-        Ok(Config { query, file_path })
+
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+        Ok(Config { query, file_path, ignore_case})
     }
 }
 
 fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    for line in search(&config.query, &contents) {
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{line}");
     }
 
@@ -33,15 +42,16 @@ fn main() {
     let args: Vec<String> = env::args().collect(); // collect() is one of those rare functions in rust where you need to annotate
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing argument!:\n{}", err);
+        eprintln!("Problem parsing argument!:\n{}", err);
         process::exit(1);
     });
+
 
     println!("Searching for \"{}\"", config.query);
     println!("In file \"{}\"", config.file_path);
 
     run(config).unwrap_or_else(|err| {
-        println!("Application arguments error!:\n{}", err);
+        eprintln!("Application arguments error!:\n{}", err);
         process::exit(1);
     });
 }
