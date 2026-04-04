65c582df3539631448a029ea436568540463d47e
EfranH25 <efranhimel25@gmail.com>
2026-04-04 00:17:12 -0400

    fin ch 13

--- ch12-io-project-cli/minigrep/src/main.rs
+++ ch12-io-project-cli/minigrep/src/main.rs
@@ -8,14 +8,18 @@ pub struct Config {
 }
 
 impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        // cloning for simplicity > dirty way to fix ownership issues > others better ways to fix
-        if args.len() < 3 {
-            return Err("ERROR: Not  enough arguments!");
-        }
+    fn build(mut args: impl Iterator<Item = String>,) -> Result<Config, &'static str> {
+        args.next();
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+        let query = match args.next(){
+            Some(args) => args,
+            None => return Err("Didn't get a query string")
+        };
+
+        let file_path = match args.next(){
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
 
         let ignore_case = env::var("IGNORE_CASE").is_ok();
         Ok(Config { query, file_path, ignore_case})
@@ -39,10 +43,8 @@ fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect(); // collect() is one of those rare functions in rust where you need to annotate
-
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        eprintln!("Problem parsing argument!:\n{}", err);
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
--- ch13-iterators-closures/src/main.rs
+++ ch13-iterators-closures/src/main.rs
@@ -1,3 +1,5 @@
+use std::thread;
+
 #[derive(Debug, PartialEq, Copy, Clone)]
 enum ShirtColor {
     Red,
@@ -49,4 +51,17 @@ fn main() {
         "The user with preference {:?} gets {:?}",
         user_pref2, giveaway2
     );
+
+    let mut list = vec![1,2,3];
+    println!("Before defining closure: {list:?}");
+
+    let mut borrows_mutably = || list.push(7);
+    borrows_mutably();
+    println!("After calling closure: {list:?}");
+
+    let list = vec![1,2,3];
+    println!("Before defining closure: {list:?}");
+    thread::spawn(move || println!("From thread: {list:?}")).join().unwrap();
+
+
 }
