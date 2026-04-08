be3f72e4677cda00b4ae939586c5b7746b75a431
EfranH25 <efranhimel25@gmail.com>
2026-04-08 01:15:59 -0400

    fin ch 15

--- ch15-smart-pointers/src/main.rs
+++ ch15-smart-pointers/src/main.rs
@@ -1,11 +1,12 @@
 use std::ops::Deref;
 
 enum List {
-    Cons(i32, Box<List>),
+    Cons(i32, Rc<List>),
     Nil
 }
 
 use crate::List::{Cons, Nil};
+use std::rc::Rc;
 
 struct MyBox<T>(T);
 
@@ -28,11 +29,35 @@ fn hello(name: &str) {
     println!("Hello {name}!")
 }
 
+
+struct CustomSmartPointer  {
+    data: String
+}
+
+impl Drop for CustomSmartPointer  {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
+}
+
+
+
 fn main() {
     let b = Box::new(5);
     println!("b = {b}");
 
-    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    // old list using Box
+    // let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    let b = Cons(3, Rc::clone(&a)); //Rc::clone does not make a deep copy. Increases reference count
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 
     let x = 5;
     let y = &x;
@@ -46,4 +71,14 @@ fn main() {
 
     let m = MyBox::new(String::from("Rust"));
     hello(&m);
+
+    let c = CustomSmartPointer{
+        data: String::from("my stuff")
+    };
+    let d = CustomSmartPointer{
+        data: String::from("other stuff")
+    };
+    println!("CustomerSmartPointers created!");
+    drop(c);
+    println!("CustomSmartPointer dropped before the end of main");
 }
