93b74d5c6e900e24c571eaec88a230cedb804621
efran <efranhimel25@gmail.com>
2026-04-25 15:13:42 -0400

    fin async chapter

--- ch17-async/src/main.rs
+++ ch17-async/src/main.rs
@@ -1,4 +1,6 @@
-use trpl::{Either, Html};
+use std::thread;
+use std::time::Duration;
+use trpl::{Either, Html, StreamExt};
 
 async fn page_title(url: &str) -> (&str, Option<String>) {
     let response_text = trpl::get(url).await.text().await;
@@ -8,13 +10,13 @@ async fn page_title(url: &str) -> (&str, Option<String>) {
     (url, title)
 }
 
-async fn example_async(){
+async fn example_async() {
     let args: Vec<String> = std::env::args().collect();
 
     let title_fut_1 = page_title(&args[1]);
     let title_fut_2 = page_title(&args[2]);
 
-    let (url, maybe_title)  = match trpl::select(title_fut_1, title_fut_2).await {
+    let (url, maybe_title) = match trpl::select(title_fut_1, title_fut_2).await {
         Either::Left(left) => left,
         Either::Right(right) => right,
     };
@@ -25,7 +27,188 @@ async fn example_async(){
         None => println!("It had no title."),
     }
 }
+
+fn concur_ex_1() {
+    trpl::block_on(async {
+        let handle = trpl::spawn_task(async {
+            for i in 1..10 {
+                println!("hi number {i} from the first task!");
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        });
+
+        for i in 1..5 {
+            println!("hi number {i} from the second task!");
+            trpl::sleep(Duration::from_millis(500)).await;
+        }
+
+        handle.await.unwrap();
+    })
+}
+
+fn concur_ex_2() {
+    // With threads, the operating system decides which thread to check and how long to let it run. With async Rust, the runtime decides which task to check. (In practice, the details get complicated because an async runtime might use operating system threads under the hood as part of how it manages concurrency, so guaranteeing fairness can be more work for a runtime—but it’s still possible!) Runtimes don’t have to guarantee fairness for any given operation, and they often offer different APIs to let you choose whether or not you want fairness.
+    trpl::block_on(async {
+        let fut1 = async {
+            for i in 1..10 {
+                println!("hi number {i} from the first task!");
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        };
+
+        let fut2 = async {
+            for i in 1..5 {
+                println!("hi number {i} from the second task!");
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        };
+
+        trpl::join(fut1, fut2).await;
+    })
+}
+
+fn example_async_msg_pass() {
+    trpl::block_on(async {
+        let (tx, mut rx) = trpl::channel();
+
+        let tx1 = tx.clone();
+        let tx1_fut = async move {
+            let vals = vec![
+                String::from("hi"),
+                String::from("from"),
+                String::from("the"),
+                String::from("future"),
+            ];
+
+            for val in vals {
+                tx1.send(val).unwrap();
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        };
+
+        let rx_fut = async {
+            while let Some(value) = rx.recv().await {
+                println!("received '{value}'");
+            }
+        };
+
+        let tx_fut = async move {
+            let vals = vec![
+                String::from("more"),
+                String::from("messages"),
+                String::from("for"),
+                String::from("you"),
+            ];
+
+            for val in vals {
+                tx.send(val).unwrap();
+                trpl::sleep(Duration::from_millis(1500)).await;
+            }
+        };
+
+        trpl::join!(tx1_fut, tx_fut, rx_fut);
+    })
+}
+
+fn slow(name: &str, ms: u64) {
+    thread::sleep(Duration::from_millis(ms));
+    println!("'{name} ran for {ms}ms'")
+}
+
+fn example_thread_block() {
+    trpl::block_on(async {
+        // let one_ms = Duration::from_millis(1);
+
+        let a = async {
+            println!("'a' started.");
+            slow("a", 30);
+            trpl::yield_now().await;
+            slow("a", 10);
+            trpl::yield_now().await;
+            slow("a", 20);
+            trpl::yield_now().await;
+            println!("'a' finished.");
+        };
+
+        let b = async {
+            println!("'b' started.");
+            slow("b", 75);
+            trpl::yield_now().await;
+            slow("b", 10);
+            trpl::yield_now().await;
+            slow("b", 15);
+            trpl::yield_now().await;
+            slow("b", 350);
+            trpl::yield_now().await;
+            println!("'b' finished.");
+        };
+
+        trpl::select(a, b).await;
+    })
+}
+
+async fn timeout<F: Future>(future_to_try: F, max_time: Duration) -> Result<F::Output, Duration> {
+    match trpl::select(future_to_try, trpl::sleep(max_time)).await {
+        Either::Left(output) => Ok(output),
+        Either::Right(_) => Err(max_time),
+    }
+}
+fn example_abstract_async() {
+    trpl::block_on(async {
+        let slow = async {
+            trpl::sleep(Duration::from_secs(5)).await;
+            "Finally finished"
+        };
+
+        match timeout(slow, Duration::from_secs(2)).await {
+            Ok(message) => println!("Succeeded with '{message}'"),
+            Err(duration) => {
+                println!("Failed after {} seconds", duration.as_secs())
+            }
+        }
+    })
+}
+
+fn example_future_streams() {
+    trpl::block_on(async {
+        let values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        let iter = values.iter().map(|n| n * 2);
+        let mut stream = trpl::stream_from_iter(iter);
+
+        while let Some(value) = stream.next().await {
+            println!("The value was: {value}");
+        }
+    })
+}
+
+fn example_thread_async(){
+    // When thinking about which method to use when, consider these rules of thumb:
+    // If the work is very parallelizable (that is, CPU-bound), such as processing a bunch of data where each part can be processed separately, threads are a better choice.
+    // If the work is very concurrent (that is, I/O-bound), such as handling messages from a bunch of different sources that may come in at different intervals or different rates, async is a better choice.
+
+    let (tx, mut rx) = trpl::channel();
+
+    thread::spawn(move || {
+        for i in 1..11 {
+            tx.send(i).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    trpl::block_on(async {
+        while let Some(message) = rx.recv().await {
+            println!("'{message}' received!");
+        }
+    });
+}
 fn main() {
-    trpl::block_on(example_async());
+    // trpl::block_on(example_async());
+    // concur_ex_1();
+    // concur_ex_2();
+    // example_async_msg_pass();
+    // example_thread_block();
+    // example_abstract_async();
+    // example_future_streams();
+    example_thread_async();
 
 }
